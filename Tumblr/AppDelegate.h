4d8f98f4f5279ab04072769b6d6ca4920c496416
Ilana Shapiro <ilanashapiro@fb.com>
2019-06-27 10:29:33 -0700

    Initial Commit

---
+++ Tumblr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tumblr
+//
+//  Created by ilanashapiro on 6/27/19.
+//  Copyright © 2019 ilanashapiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
